bd20654b62db4fa978fa0006238bb2748def63cf
oriori5 <oriori5@gmail.com>
2015-09-19 13:31:48 +0300

    Added the helper for the admin manager page

---
+++ ShaulisCrazyFans/Helpers/ProtectedHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShaulisCrazyFans.Helpers
+{
+    public class AdminMembershipAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            //redirect if not authenticated
+            if (filterContext.HttpContext.Session["Admin-Authentication"] == null ||
+                filterContext.HttpContext.Session["Admin-Authentication"] != "true")
+            {
+                //use the current url for the redirect
+                string redirectOnSuccess = filterContext.HttpContext.Request.Url.AbsolutePath;
+
+                //send them off to the login page
+                string loginUrl = "/PostManager/Login";
+                filterContext.HttpContext.Response.Redirect(loginUrl, true);
+            }
+        }
+    }
+}
